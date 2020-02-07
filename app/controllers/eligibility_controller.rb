a9d40a1b0c9e91367fb10cc9683ba8a769d538d8
Karl Baker <karl.baker@digital.cabinet-office.gov.uk>
2020-02-07 11:09:37 +0000

    Add EligibilityPresenter

--- app/controllers/eligibility_controller.rb
+++ app/controllers/eligibility_controller.rb
@@ -1,5 +1,5 @@
 class EligibilityController < ApplicationController
   def index
-
+    @presenter = EligibilityPresenter.new
   end
 end
---
+++ app/presenters/eligibility_presenter.rb
@@ -0,0 +1,7 @@
+class EligibilityPresenter
+  attr_reader :title
+
+  def initialize
+    @title = 'Eligibility Viewer'
+  end
+end
